87c9ba83d71d92ecd8c420ecec1eae09d1fe2ada
Abdumalik <48346947+python-pro@users.noreply.github.com>
2019-05-28 19:34:18 -0500

    Create vid_3_snippet_1.py

---
+++ vid_3_snippet_1.py
@@ -0,0 +1,35 @@
+# B_R_R
+# M_S_A_W
+
+# This project prompt you for a input and encrypts it and then decrypts back.
+# Uppercase, lowercase and white space are considered.
+
+#A-Z 65-97
+#a-z 97-122
+
+
+norm_string = input("Enter a string to hide in uppercase: ")
+ 
+secret_string = ""
+ 
+# Cycle through each character in the string
+for char in norm_string:
+ 
+    # Store each character code in a new string
+  secret_string += str(ord(char)+100)
+ 
+print("Secret Message :", secret_string)
+ 
+norm_string = ""
+ 
+# Cycle through each character code 2 at a time by incrementing by
+# 2 each time through since unicodes go from 65 to 90
+for i in range(0, len(secret_string)-1, 3):
+ 
+    # Get the 1st and 2nd for the 2 digit number
+    char_code = secret_string[i] + secret_string[i+1]+secret_string[i+2]
+ 
+    # Convert the codes into characters and add them to the new string
+    norm_string += chr(int(char_code)-100)
+ 
+print("Original Message :", norm_string)
